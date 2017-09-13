a385f88aa4d9d4d323814caa503f81af091b713a
Elvin <ttop34@icloud.com>
2017-09-13 11:31:54 -0500

    install jquery and modified the DOM with it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const $ = require("jquery");
+
 const hello = () => console.log('hello from webpack!');
 
 hello();
+
+$('body')
+    .html("<h1>Hello from JQuery</h1>")
+    .css('background-color', 'grey');
